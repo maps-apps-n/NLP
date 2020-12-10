b689f54f9f74f8acf9b26b985a1ad7955a78c8ad
Nicolas <saravianic09@gmail.com>
2020-12-10 15:48:44 -0500

    Add files via upload

---
+++ basic_sentiment.py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analysis = TextBlob("TextBlob sure looks like it has some interesting features")
+
+print(dir(analysis))
+
+print(analysis.sentiment)
+
+
+
+
+
